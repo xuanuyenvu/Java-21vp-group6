949798aa8f8920d6b2f981d52e5e99c8a9b21548
phivu3125 <phivu3125@gmail.com>
2024-02-29 22:57:30 +0700

    add bookcrud ui

---
+++ src/main/java/com/group06/bsms/books/ActionPanel.java
@@ -0,0 +1,91 @@
+package com.group06.bsms.books;
+
+import com.group06.bsms.utils.SVGHelper;
+import java.awt.Color;
+
+
+public class ActionPanel extends javax.swing.JPanel {
+
+    public ActionPanel() {
+        initComponents();
+        
+
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        editBtn = new javax.swing.JButton();
+        hideBtn = new javax.swing.JButton();
+
+        setToolTipText("");
+        setPreferredSize(new java.awt.Dimension(97, 25));
+
+        editBtn.setIcon(SVGHelper.createSVGIconWithFilter(
+            "icons/search.svg",
+            Color.black, Color.black,
+            14, 14
+        ));
+        editBtn.setToolTipText("Edit");
+        editBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        editBtn.setPreferredSize(new java.awt.Dimension(25, 25));
+        editBtn.setRolloverEnabled(false);
+        editBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                editBtnActionPerformed(evt);
+            }
+        });
+
+        hideBtn.setIcon(SVGHelper.createSVGIconWithFilter(
+            "icons/eye.svg",
+            Color.black, Color.black,
+            14, 14
+        ));
+        hideBtn.setToolTipText("Hide");
+        hideBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        hideBtn.setPreferredSize(new java.awt.Dimension(25, 25));
+        hideBtn.setRolloverEnabled(false);
+        hideBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                hideBtnActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(13, Short.MAX_VALUE)
+                .addComponent(editBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(hideBtn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(16, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(editBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(hideBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void editBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editBtnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_editBtnActionPerformed
+
+    private void hideBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_hideBtnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_hideBtnActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton editBtn;
+    private javax.swing.JButton hideBtn;
+    // End of variables declaration//GEN-END:variables
+}
--- src/main/java/com/group06/bsms/books/BookCRUD.java
+++ src/main/java/com/group06/bsms/books/BookCRUD.java
@@ -1,39 +1,184 @@
 package com.group06.bsms.books;
 
+import com.formdev.flatlaf.FlatClientProperties;
+import com.group06.bsms.utils.SVGHelper;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
+
 public class BookCRUD extends javax.swing.JPanel {
 
     public BookCRUD() {
         initComponents();
+
+        searchBar.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "Search");
+        searchBar.putClientProperty(FlatClientProperties.TEXT_FIELD_LEADING_ICON, SVGHelper.createSVGIconWithFilter(
+                "icons/search.svg",
+                Color.black, Color.black,
+                14, 14
+        ));
+        
+        table.getColumnModel().getColumn(5).setCellRenderer(new TableActionCellRender());
+        table.getTableHeader().setFont(new java.awt.Font("Segoe UI", 0, 16));
+        table.setShowVerticalLines(true);
+        
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        TableRowSorter<DefaultTableModel> sorter = new TableRowSorter<>(model);
+        table.setRowSorter(sorter);
+        sorter.setSortable(0, false);
+        sorter.setSortable(1, false);
+        sorter.setSortable(2, false);
+        sorter.setSortable(5, false);
+        
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+        table.getColumnModel().getColumn(3).setCellRenderer(centerRenderer);
+        table.getColumnModel().getColumn(4).setCellRenderer(centerRenderer);        
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        label = new javax.swing.JLabel();
+        bookLabel = new javax.swing.JLabel();
+        searchBar = new javax.swing.JTextField();
+        createBtn = new javax.swing.JButton();
+        filterBtn = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        table = new javax.swing.JTable();
 
-        label.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
-        label.setText("Book CRUD");
+        bookLabel.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        bookLabel.setText("BOOKS");
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap(148, Short.MAX_VALUE)
-                .addComponent(label)
-                .addContainerGap(170, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap(125, Short.MAX_VALUE)
-                .addComponent(label)
-                .addContainerGap(153, Short.MAX_VALUE))
-        );
+        searchBar.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
+        searchBar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                searchBarActionPerformed(evt);
+            }
+        });
+
+        createBtn.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
+        createBtn.setIcon(SVGHelper.createSVGIconWithFilter(
+            "icons/add.svg",
+            Color.black, Color.black,
+            14, 14
+        ));
+        createBtn.setText("Create");
+        createBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        createBtn.setHorizontalAlignment(javax.swing.SwingConstants.LEADING);
+        createBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                createBtnActionPerformed(evt);
+            }
+        });
+
+        filterBtn.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
+        filterBtn.setIcon(SVGHelper.createSVGIconWithFilter(
+            "icons/filter.svg",
+            Color.black, Color.black,
+            14, 14));
+    filterBtn.setText("Filter");
+    filterBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+    filterBtn.setHorizontalAlignment(javax.swing.SwingConstants.LEADING);
+    filterBtn.addActionListener(new java.awt.event.ActionListener() {
+        public void actionPerformed(java.awt.event.ActionEvent evt) {
+            filterBtnActionPerformed(evt);
+        }
+    });
+
+    table.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
+    table.setModel(new javax.swing.table.DefaultTableModel(
+        new Object [][] {
+            {null, null, null,  new Integer(1), null, null},
+            {null, null, null,  new Integer(2), null, null}
+        },
+        new String [] {
+            "Title", "Author", "Publisher", "Quantity", "Sale price", "Action"
+        }
+    ) {
+        Class[] types = new Class [] {
+            java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.Double.class, java.lang.Object.class
+        };
+        boolean[] canEdit = new boolean [] {
+            false, false, false, false, false, false
+        };
+
+        public Class getColumnClass(int columnIndex) {
+            return types [columnIndex];
+        }
+
+        public boolean isCellEditable(int rowIndex, int columnIndex) {
+            return canEdit [columnIndex];
+        }
+    });
+    table.setToolTipText("");
+    table.setRowHeight(40);
+    table.getTableHeader().setReorderingAllowed(false);
+    jScrollPane1.setViewportView(table);
+    if (table.getColumnModel().getColumnCount() > 0) {
+        table.getColumnModel().getColumn(0).setMinWidth(150);
+        table.getColumnModel().getColumn(3).setPreferredWidth(20);
+        table.getColumnModel().getColumn(4).setPreferredWidth(20);
+        table.getColumnModel().getColumn(5).setPreferredWidth(50);
+    }
+
+    javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+    this.setLayout(layout);
+    layout.setHorizontalGroup(
+        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        .addGroup(layout.createSequentialGroup()
+            .addGap(42, 42, 42)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addComponent(bookLabel)
+                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 836, Short.MAX_VALUE)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(searchBar, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(48, 48, 48)
+                            .addComponent(createBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(filterBtn)))
+                    .addGap(50, 50, 50))))
+    );
+    layout.setVerticalGroup(
+        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        .addGroup(layout.createSequentialGroup()
+            .addGap(48, 48, 48)
+            .addComponent(bookLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGap(18, 18, 18)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addComponent(searchBar, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(createBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(filterBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGap(18, 18, 18)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 419, Short.MAX_VALUE)
+            .addGap(30, 30, 30))
+    );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void searchBarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchBarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_searchBarActionPerformed
+
+    private void createBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_createBtnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_createBtnActionPerformed
+
+    private void filterBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_filterBtnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_filterBtnActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel label;
+    private javax.swing.JLabel bookLabel;
+    private javax.swing.JButton createBtn;
+    private javax.swing.JButton filterBtn;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField searchBar;
+    private javax.swing.JTable table;
     // End of variables declaration//GEN-END:variables
 }
