6e82cd1fc7ee3c9ea2e8bff6bd2d06a612c620d7
vtcong21 <vtcong21@vp.fitus.edu.vn>
2024-04-17 08:41:45 +0700

    import sheet table molder

--- src/main/java/com/group06/bsms/importsheet/ImportSheetTableModel.java
+++ src/main/java/com/group06/bsms/importsheet/ImportSheetTableModel.java
@@ -8,6 +8,7 @@ import javax.swing.table.AbstractTableModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ImportSheetTableModel extends AbstractTableModel {
     private List<ImportSheet> importSheets = new ArrayList<>();
@@ -50,4 +51,77 @@ public class ImportSheetTableModel extends AbstractTableModel {
                 return null;
         }
     }
+
+    @Override
+    public void setValueAt(Object val, int row, int col) {
+
+    }
+
+    public ImportSheet getImportSheet(int row) {
+        return importSheets.get(row);
+    }
+
+    public String getColumnName(int col) {
+        return columns[col];
+    }
+
+    public boolean contains(int id) {
+        Optional<ImportSheet> foundBook = importSheets.stream()
+                .filter(importSheet -> importSheet.id == id)
+                .findFirst();
+        return foundBook.isPresent();
+    }
+
+    @Override
+    public Class<?> getColumnClass(int col) {
+        switch (col) {
+            case 0:
+                return String.class;
+            case 1:
+                return String.class;
+            case 2:
+                return String.class;
+            case 3:
+                return Integer.class;
+            case 4:
+                return Double.class;
+            case 5:
+                return Boolean.class;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+    public void reloadAllImportSheets(List<ImportSheet> newImportSheets) {
+        if (newImportSheets != null) {
+            importSheets.clear();
+            fireTableDataChanged();
+            for (var importSheet : newImportSheets) {
+                if (!contains(importSheet.id)) {
+                    addRow(importSheet);
+                }
+            }
+        }
+
+    }
+
+    public void loadNewBooks(List<ImportSheet> newImportSheets) {
+        if (newImportSheets != null) {
+            for (var importSheet : newImportSheets) {
+                if (!contains(importSheet.id)) {
+                    addRow(importSheet);
+                }
+            }
+        }
+    }
+
+    private void addRow(ImportSheet importSheet) {
+        importSheets.add(importSheet);
+    }
+
 }
